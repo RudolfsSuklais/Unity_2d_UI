bf2d67968fdc623e57c2c0488cef56006a067693
Rudolfs <ruudisrudolfs@gmail.com>
2024-05-09 09:18:38 +0300

    Pievienoju pogu

---
+++ Assets/Scenes/Script/ShowTextScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowTextScript : MonoBehaviour
+{
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Uzstartēts!");
+    }
+
+    public int kadrs = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        kadrs++;
+        Debug.Log(kadrs);
+    }
+    */
+
+    string text;
+    public GameObject inputField;
+    public GameObject displayField;
+
+    public void getText()
+    {
+        text = inputField.GetComponent<Text>().text;
+        displayField.GetComponent<Text>().text = "Sveiks " + text.ToUpper() + "!";
+    }
+}
